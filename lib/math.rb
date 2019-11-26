f8363e7efc7e25b24fe9ab06555e70365bd53657
Tomas Tobar <codetombomb@gmail.com>
2019-11-26 21:46:07 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,9 +6,7 @@ def subtraction(num1, num2)
     num1 - num2
 end
 
-def division
-  num1 = 34.0
-  num2 = 5
+def division (num1, num2)
   num1 / num2 
 end
 
@@ -16,8 +14,10 @@ def multiplication(num1, num2)
   num1 * num2
 end
 
-def modulo(num1, num2)
- Float(num1 / num2)
+def modulo
+  num1 = 34.0
+  num2 = 5
+ num1 / num2
 end
 
 def square_root(num)
